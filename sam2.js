eb409938cc5a7cb38ce64dbd70985d04f571c0c0
vantakulakrupanandh24csm-lgtm <vantakulakrupanandh.24.csm@anits.edu.in>
2026-09-17 14:32:34 +0530

    Add files via upload

---
+++ sam2.js
@@ -0,0 +1,34 @@
+const x=10;
+const y=20;
+x+y
+function add(a,b){
+function add(a,b){return a+b;
+function add(a,b){return a+b;}
+add(30,13)
+_*2
+3+3
+_*2
+const x=10;
+const y=20;
+x+y
+function add(a,b){
+function add(a,b){
+return a+b;
+function add(a,b){
+return a+b;
+}
+add(30,13)
+_*2
+3+3
+_*2
+_*2
+_*2
+const fs=require('fs');
+fs.readFileSync('text.txt','utf8');
+Math.ceil(10/3)
+Math.floor(10/3)
+Math.round(Math.e,3)
+Math.round(2.57362,3)
+Math.pow(3,4)
+var a=4;
+a
